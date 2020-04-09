215d0e5f5ed54193dea33e67676fad3eab1922df
myskip <myskip@163.com>
2020-04-09 22:22:47 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 if [ $# -eq "0" ]; then
     cd server
     ./build.sh debug rec
+    cd client
+    ./build.sh debug rec
 elif [ $1 == "clean" ]; then
     cd server
+    ./build.sh clean
+    cd client
     ./build.sh clean
 else
     echo "params error"
